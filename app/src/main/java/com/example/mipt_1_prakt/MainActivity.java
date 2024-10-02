b193ec20c0dd92efdbe8bca7351d19927127b71c
OrangeTree33 <krakaduzaz2@gmail.com>
2024-10-02 17:57:01 +0300

    Second button

--- app/src/main/java/com/example/mipt_1_prakt/MainActivity.java
+++ app/src/main/java/com/example/mipt_1_prakt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mipt_1_prakt;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView tvMain;
     Button btnChangeText;
+    Button btnChangeColour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,13 +32,22 @@ public class MainActivity extends AppCompatActivity {
 
         });
         btnChangeText = findViewById(R.id.btnChangeText);
-
+        btnChangeColour = findViewById(R.id.btnChangeColour);
         btnChangeText.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 tvMain = (TextView)findViewById(R.id.tvMain);
                 tvMain.setText("Hello");
+            }
+        });
+        btnChangeColour.setOnClickListener(new View.OnClickListener() {
+            Random rnd = new Random();
+            @Override
+            public void onClick(View view) {
+                tvMain = (TextView)findViewById(R.id.tvMain);
+                tvMain.setTextColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)));
+
             }
         });
 
